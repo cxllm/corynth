9f22901562ce480ed3952724b39e334d654f2fd0
Callum <cxllm@outlook.fr>
2021-03-21 13:14:47 +0000

    Rewrite

--- Events/ready.ts
+++ Events/ready.ts
@@ -30,19 +30,17 @@ export = class extends Event {
         setInterval(() => {
             this.client.Util.tokenPush();
         }, 60 * 1000)
-        setInterval(() => {
-            this.client.users.cache.clear();
-            this.client.db.guilds.clearCache();
-        }, 10 * 60 * 1000)
         setTimeout(async () => {
             this.client.user.setActivity(activity);
             await this.client.Util.updateCovid19Info();
             if (this.client.user.id !== "692779290399604766") await this.client.Util.botLists();
             await this.checkUsers()
+            this.client.users.cache.clear();
+            this.client.db.guilds.clearCache();
         }, 30 * 60 * 1000)
     }
     async checkUsers() {
-        const data = await this.client.db.users.collection.find({});
+        const data = this.client.db.users.collection.find({});
         data.on("data", async (data: { id: string, cooldown: Array<{ end: number }> }) => {
             data.cooldown.map(cooldown => {
                 if (cooldown.end < Date.now()) data.cooldown.splice(data.cooldown.indexOf(cooldown), 1)
