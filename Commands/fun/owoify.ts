9d3a9a87f1d62fb293594e5c94e5dcca71cec3c4
Callum <cxllm@outlook.fr>
2021-05-08 08:06:22 +0100

    Fixed owoify

--- Commands/fun/owoify.ts
+++ Commands/fun/owoify.ts
@@ -1,33 +1,39 @@
 import Client from "../../Structs/Client";
 import Command from "../../Structs/Command";
 import Message from "../../Structs/Message";
+
 export = class extends Command {
-  private client: Client;
-  constructor(client: Client) {
-    super(
-      "owoify",
-      {
-        description: "OwOify some text.",
-        aliases: ["owo"],
-        usage: "<text>",
-      },
-      {
-        owner: false,
-        args: 1,
-        permissions: {},
-      }
-    );
-    this.client = client;
-  }
-  async run(msg: Message) {
-    const text = encodeURIComponent(msg.args.join(" "));
-    const { data } = await this.client.web.get(
-      `https://nekos.life/api/v2/owoify?text=${text}`
-    );
-    let embed = {
-      description: data.owo,
-      color: this.client.config.colours.main,
-    };
-    await msg.send({ embed });
-  }
+    private client: Client;
+
+    constructor(client: Client) {
+        super(
+            "owoify",
+            {
+                description: "OwOify some text.",
+                aliases: ["owo"],
+                usage: "<text>",
+            },
+            {
+                owner: false,
+                args: 1,
+                permissions: {},
+            }
+        );
+        this.client = client;
+    }
+
+    async run(msg: Message) {
+        const text = encodeURIComponent(msg.args.join(" "));
+        const {data} = await this.client.web.get(
+            `https://nekos.life/api/v2/owoify?text=${text}`
+        );
+        if (data.msg) {
+            return await msg.send("Text is too long!")
+        }
+        let embed = {
+            description: data.owo,
+            color: this.client.config.colours.main,
+        };
+        await msg.send({embed});
+    }
 };
