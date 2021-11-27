98970c0b831a35d5cc6cded3d38f6bdf52780bf4
Callum <59397232+cxllm@users.noreply.github.com>
2021-11-27 20:14:28 +0000

    Updated domain

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -40,7 +40,7 @@ export default class Navigation extends React.Component<{
 									<NavDropdown.Item href="https://twitter.com/CX11M">
 										Twitter
 									</NavDropdown.Item>
-									<NavDropdown.Item href="https://blog.cxllm.cf">Blog</NavDropdown.Item>
+									<NavDropdown.Item href="https://blog.cxllm.com">Blog</NavDropdown.Item>
 								</NavDropdown>
 								<Nav.Link onClick={this.props.setLang}>{translation.language}</Nav.Link>
 							</Nav>
